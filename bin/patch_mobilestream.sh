042fa6d8e6df3bf45523264addcd8c658aaca0a2
Jon <jon_larrea@hotmail.com>
2020-09-06 18:09:32 +0100

    Fixed DB creating

--- bin/patch_mobilestream.sh
+++ bin/patch_mobilestream.sh
@@ -19,7 +19,8 @@ sed -i -e '29s/INTEGRITY_ALGORITHM_ID_128_EIA2/INTEGRITY_ALGORITHM_ID_128_EIA1/'
 rm /opt/mobilestream-conext/mobilestreamconext/MobileStream-C++/mobilestream/src/block/*-e
 
 echo 'Populating HSS DB with 4096 UEs...'
-sudo bash /opt/mobilestream-conext/mobilestreamconext/testbed/hss/provision.sh 4096
+cd /opt/mobilestream-conext/mobilestreamconext/testbed/hss/
+sudo bash provision.sh 4096
 
 cd /opt/mobilestream-conext/mobilestreamconext/MobileStream-C++/build/
 sudo cmake ../

eb5f38553bc95c1b71019e5bafd1ced5c9545053
Jon <jon_larrea@hotmail.com>
2021-02-12 15:24:03 +0100

    Reboot removed

--- scripts/free5gc_stage1.sh
+++ scripts/free5gc_stage1.sh
@@ -7,5 +7,3 @@ sudo apt -y install git build-essential strace net-tools iputils-ping iproute2 m
 sudo apt -y install linux-image-5.0.0-23-generic linux-modules-5.0.0-23-generic linux-headers-5.0.0-23-generic
 sudo grub-set-default 1
 sudo update-grub
-
-sudo reboot

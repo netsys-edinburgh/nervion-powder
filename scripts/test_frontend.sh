dbe71210b30e2681e0cb8fd00cbdd27f89fdf87f
Jon <jon_larrea@hotmail.com>
2021-03-30 13:40:35 +0200

    test files fixed

--- scripts/test_frontend.sh
+++ scripts/test_frontend.sh
@@ -6,7 +6,7 @@ sudo apt install -y libsctp-dev build-essential make
 
 cd ~
 
-git clone https://j0lama:0b1fe6bbb294fd9d4462f0ac798880d82250d6a8@github.com/j0lama/CoreKube.git
+git clone https://j0lama:37962f1338ebfbfad0184e289106a000adcf00bb@github.com/j0lama/CoreKube.git
 
 cd CoreKube/
 make

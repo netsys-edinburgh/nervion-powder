fd178edceab4676486e49b11238357e38ca92283
andrewferguson <andrewferguson500@gmail.com>
2021-08-04 22:36:07 +0100

    Use tcpdump to log all traffic - Nervion master

--- scripts/master.sh
+++ scripts/master.sh
@@ -137,6 +137,9 @@ sudo kubectl create -f config/test/metrics-server.yaml
 # Deploy emulator
 sudo kubectl create -f config/deployment.yaml
 
+# Log all the traffic on the Nervion master node
+sudo tcpdump -i any -w ~/tcpdump.pcap &
+
 # to know how much time it takes to instantiate everything.
 echo "Setup DONE!"
 date

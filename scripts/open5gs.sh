f9bb6f42730ff522384be186a32f3096e67c4fb0
j0lama <jon_larrea@hotmail.com>
2021-08-19 12:44:01 +0100

    Open5GS 2.1.7 working with Nervion

--- scripts/open5gs.sh
+++ scripts/open5gs.sh
@@ -1,45 +1,52 @@
 #!/bin/bash
 
-exit 0
+# Update repository
+echo "Updating repositories..."
+sudo apt -y update
 
-echo "Installing Open5GS"
-sudo apt update
-sudo apt -y install software-properties-common
-sudo add-apt-repository ppa:open5gs/latest
-sudo apt update
-sudo apt -y install open5gs
+# Install MongoDB
+echo "Installing MongoDB..."
+sudo apt -y install mongodb
+sudo systemctl start mongodb
+sudo systemctl enable mongodb
 
-echo "Installing webUI"
-sudo apt -y install curl
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt -y install nodejs
-curl -sL https://open5gs.org/open5gs/assets/webui/install | sudo -E bash -
+# Configure TUN interface
+echo "Configuring TUN interface..."
+sudo ip tuntap add name ogstun mode tun
+sudo ip addr add 10.45.0.1/16 dev ogstun
+sudo ip addr add 2001:230:cafe::1/48 dev ogstun
+sudo ip link set ogstun up
+
+# Install dependencies
+echo "Installing dependencies..."
+sudo apt -y install python3-pip python3-setuptools python3-wheel ninja-build build-essential flex bison git libsctp-dev libgnutls28-dev libgcrypt-dev libssl-dev libidn11-dev libmongoc-dev libbson-dev libyaml-dev libnghttp2-dev libmicrohttpd-dev libcurl4-gnutls-dev libnghttp2-dev libtins-dev meson software-properties-common unzip
+
+cd ~
+
+# Download Open5GS 2.1.7
+echo "Cloning Open5GS v2.1.7..."
+wget https://github.com/open5gs/open5gs/archive/refs/tags/v2.1.7.zip
+unzip v2.1.7.zip
+mv open5gs-2.1.7/ open5gs/
+
+# Compile Open5GS
+echo "Compiling Open5GS..."
+cd open5gs/
+meson build --prefix=`pwd`/install
+ninja -C build
+
+sudo ldconfig
+
+echo "Installing Open5GS..."
+cd build
+ninja install
+cd ../
 
 echo "Adding subscribers to the DB..."
 for i in $(seq -f "%010g" 1 1500)
 do
 	echo "UE: 20893$i"
-	open5gs-dbctl add 20893$i 465B5CE8B199B49FAA5F0A2EE238A6BC E8ED289DEBA952E4283B54E88E6183CA
+	~/open5gs/misc/db/open5gs-dbctl add 20893$i 465B5CE8B199B49FAA5F0A2EE238A6BC E8ED289DEBA952E4283B54E88E6183CA
 done
 
-echo "Modifying configuration files..."
-sudo cp /local/repository/config/open5gs/amf.yaml /etc/open5gs/
-sudo cp /local/repository/config/open5gs/upf.yaml /etc/open5gs/
-
-echo "Restarting Open5GS..."
-sudo systemctl restart open5gs-mmed
-sudo systemctl restart open5gs-sgwcd
-sudo systemctl restart open5gs-smfd
-sudo systemctl restart open5gs-amfd
-sudo systemctl restart open5gs-sgwud
-sudo systemctl restart open5gs-upfd
-sudo systemctl restart open5gs-hssd
-sudo systemctl restart open5gs-pcrfd
-sudo systemctl restart open5gs-nrfd
-sudo systemctl restart open5gs-ausfd
-sudo systemctl restart open5gs-udmd
-sudo systemctl restart open5gs-pcfd
-sudo systemctl restart open5gs-udrd
-sudo systemctl restart open5gs-webui
-
-echo "Done!"
+sudo ~/open5gs/build/tests/app/5gc -c /local/repository/config/open5gs/sample.yaml &
